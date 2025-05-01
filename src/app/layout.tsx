0cc94455f66fad36cb1ce2d2b2d11cf84245cdb2
UmeshpJadhav <Umesh2005jadhav@gmail.com>
2025-05-01 17:48:24 +0530

    button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const dmSans = DM_Sans ({
+   
+subsets: ["latin"]
+
 });
 
 export const metadata: Metadata = {
@@ -25,10 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ dmSans.className } antialiased`}
       >
         {children}
       </body>
     </html>
   );
 }
+ 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";  
+import { Progress } from "@/components/ui/progress";
+//import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home(){
   return(
-    <div className=" text-rose-500 text-2xl ">
-      <p>
-        Hello World
-      </p>
+    <div className="p-4">
+      <div className=" flex flex-col gap-y-4 ">
+      <div>
+        <Button variant="elevated">
+          I Am Button
+        </Button>
+      </div>
+      <div className="">
+        <Input placeholder="I am a input" />
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Input type="text" placeholder="I am a text area" />
+        </div>  
+      <div>
+        <Checkbox />
+      </div>
+    </div>
     </div>
   )
 }
